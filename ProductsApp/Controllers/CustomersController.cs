0bac283a30c37f85594af0f2456e71be0974351c
MattDolan <matt.r.dolan@gmail.com>
2016-01-03 11:17:29 -0700

    Web API
    This is a web API published to Azure

---
+++ ProductsApp/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        Customers[] customers = new Customers[] 
+        { 
+            new Customers { Id = 1, Company = "Home Depot" }, 
+            new Customers { Id = 2, Company = "Lowes" }, 
+            new Customers { Id = 3, Company = "Schlotzkys" } 
+        };
+
+        public IEnumerable<Customers> GetAllProducts()
+        {
+            return customers;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var customer = customers.FirstOrDefault((p) => p.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+    }
+}
---
+++ ProductsApp/Models/Customers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string Company { get; set; }
+    }
+}
